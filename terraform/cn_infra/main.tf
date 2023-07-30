da4c594d984cd9587f9d47331ea0184df29d650e
samrat <samrat@codengine.co>
2023-07-31 02:25:23 +0530

    imp

--- terraform/cn_infra/main.tf
+++ terraform/cn_infra/main.tf
@@ -10,3 +10,28 @@ resource "aws_instance" "example" {
         name = "qa1_server"
     }
 }
+
+resource "aws_security_group" "sg-1" {
+    name = "sg-1"
+    description = "ports 80, 22 and 443 will be open to all traffic"
+    ingress {
+        cidr_blocks = ["0.0.0.0/0"]
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+    }
+    
+    ingress {
+        cidr_block = ["0.0.0.0/0"]
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+    }
+
+    ingress {
+        cidr_block = ["0.0.0.0/0"]
+        from_port = 443
+        to_port = 443
+        protocol = "tcp"
+    }
+}
